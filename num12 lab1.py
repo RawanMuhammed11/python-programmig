116015b9f9df49759cc0a5ab74ee45c7589596a5
RawanMuhammed11 <96721602+RawanMuhammed11@users.noreply.github.com>
2023-11-06 22:25:18 +0200

    Add files via upload

---
+++ num12 lab1.py
@@ -0,0 +1,4 @@
+num_units=int(input('please enter number of  units:'))
+num_tens=int(input('please enter number of tens:'))
+num_hundreds=int(input('please enter number of hundreds:'))
+print('3-digit number is =',num_hundreds*100 +num_tens*10 +num_units)
