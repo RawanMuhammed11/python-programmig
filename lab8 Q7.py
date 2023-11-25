558ba70d1199a41f0c19a0225fe0f877359f66ae
RawanMuhammed11 <96721602+RawanMuhammed11@users.noreply.github.com>
2023-11-25 22:09:45 +0200

    Update lab8 Q7.py

--- lab8 Q7.py
+++ lab8 Q7.py
@@ -1,18 +1,13 @@
 def reverse_string(input_string):
-
     reversed_string = ''
-
-    reversed_string = char + reversed_string
+    for char in input_string:
+        if char != ' ':
+            reversed_string = char + reversed_string
     return reversed_string
 
 def main():
-
     user_input = input("Enter your string: ")
-
-
     reversed_result = reverse_string(user_input)
-
-
     print(f"The reversed string without reversing spaces is: '{reversed_result}'")
 
 if __name__ == "__main__":
